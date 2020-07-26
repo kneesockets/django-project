ae0c8f7765993f8d46a00d5dab09cd7264423a0d
Vagrant Default User <vagrant@tanuki>
2020-07-26 03:41:58 +0000

    update

--- base/tests/test_home.py
+++ base/tests/test_home.py
@@ -1,12 +1,13 @@
 from project.django_assertions import assert_contains
 from django.urls import reverse
-from django.test import Client
 import pytest
+
 
 @pytest.fixture
 def response(client):
     response = client.get(reverse('base:home'))
     return response
+
 
 def test_home_status_code(response):
     assert response.status_code == 200
@@ -14,6 +15,7 @@ def test_home_status_code(response):
 
 def test_title(response):
     assert_contains(response, '<title>Home Page</title>')
+
 
 def test_navbrand_link(response):
     assert_contains(response, f'href="{reverse("base:home")}"')
--- base/urls.py
+++ base/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from base import views
 
 app_name = 'base'
